86490d237002edf2e674e8babfc38a39be63876f
tzahimoyal <tzmoyal@gmail.com>
2020-02-06 13:34:36 +0200

    Initial Commit

---
+++ CLDexerciseSDK/CLDexerciseSDK.h
@@ -0,0 +1,19 @@
+//
+//  CLDexerciseSDK.h
+//  CLDexerciseSDK
+//
+//  Created by tzahi moyal on 06/02/2020.
+//  Copyright © 2020 TZAHIMOYAL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CLDexerciseSDK.
+FOUNDATION_EXPORT double CLDexerciseSDKVersionNumber;
+
+//! Project version string for CLDexerciseSDK.
+FOUNDATION_EXPORT const unsigned char CLDexerciseSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CLDexerciseSDK/PublicHeader.h>
+
+
